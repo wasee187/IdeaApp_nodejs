2e90a2f280627f726809021dd3c31b8e77d2a7a2
Wasee Sarwar <waseesarwar187@gmail.com>
2021-08-13 03:06:05 +0600

    host address update

--- config/key.js
+++ config/key.js
@@ -2,14 +2,16 @@ const clientID = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
 const accountActivationSecret = process.env.ACCOUNT_ACTIVATION_SECRET_KEY;
-const host = process.env.HOST_ADDRESS;
 const resetPasskey = process.env.RESET_PASS_SECRET_KEY;
 
 let senderEmail;
+let host;
 if (process.env.NODE_ENV === 'development') {
   senderEmail = process.env.SENDER_EMAIL_DEV;
+  host = process.env.HOST_ADDRESS_DEV;
 } else if (process.env.NODE_ENV === 'production') {
   senderEmail = process.env.SENDER_EMAIL_PROD;
+  host = process.env.HOST_ADDRESS_PROD;
 }
 
 module.exports = {
